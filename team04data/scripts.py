bf1ef117923e5b9a2027098cdab3a69fd862f952
jaymsteele <118499633+jaymsteele@users.noreply.github.com>
2024-02-20 20:46:52 -0600

    Added output to a sql file rather than terminal output

--- team04data/scripts.py
+++ team04data/scripts.py
@@ -1,113 +1,101 @@
 import random
 from datetime import date, timedelta
-#Total of 1 million dollars = 1000000
-#52 weeks of sales data
-#Include two peak days (beginning of each semester)
-#Inventory for at least 20 different menu items
 
+# Redirecting output to a file
+with open("output.sql", "w") as sql_file:
+    sql_file.write("CREATE TABLE orders (hour varchar(255), day int, week int, month int, year int, item varchar(255), sale int, orderID int);\n")
 
+    menu_items = [
+        "Rev's Burger",
+        "Double Stack Cheese Burger",
+        "Classic Burger",
+        "Bacon Chesseburger",
+        "Three Tender Basket",
+        "Four Steak Finger Basket",
+        "Gig'Em Patty Melt",
+        "Howdy Spicy Ranch Chicken Strip Sandwich",
+        "Classic Crispy or Grilled Chicken Tender Sandwich",
+        "Grilled Cheese",
+        "Aggie Shake",
+        "Double Scoop Ice Cream Cup",
+        "Chocolate Chip Chunk Cookie",
+        "Chocolate Fudge Brownie",
+        "Salad Bar",
+        "Gig'Em Sauce",
+        "Buffalo",
+        "Ranch",
+        "BBQ Sauce",
+        "Honey Mustard",
+        "Spicy Ranch",
+        "Fountain Drink",
+        "Drip Coffee",
+        "Cold Brew",
+        "Seasoned Fries",
+        "Tater Tots",
+        "Onion Rings",
+        "Kettle Chips"
+    ]
 
-print("CREATE TABLE orders (hour varchar(255), day int, week int, month int, year int, item varchar(255), sale int, orderID int);")
+    menu_prices = {
+        "Rev's Burger": 5.59,
+        "Double Stack Cheese Burger": 8.79,
+        "Classic Burger": 5.49,
+        "Bacon Chesseburger": 6.99,
+        "Three Tender Basket": 6.79,
+        "Four Steak Finger Basket": 7.29,
+        "Gig'Em Patty Melt": 6.29,
+        "Howdy Spicy Ranch Chicken Strip Sandwich": 6.99,
+        "Classic Crispy or Grilled Chicken Tender Sandwich": 5.79,
+        "Grilled Cheese": 3.49,
+        "Aggie Shake": 3.99,
+        "Double Scoop Ice Cream Cup": 2.99,
+        "Chocolate Chip Chunk Cookie": 1.99,
+        "Chocolate Fudge Brownie": 1.99,
+        "Salad Bar": 8.99,
+        "Gig'Em Sauce": 0.69,
+        "Buffalo": 0.69,
+        "Ranch": 0.69,
+        "BBQ Sauce": 0.69,
+        "Honey Mustard": 0.69,
+        "Spicy Ranch": 0.69,
+        "Fountain Drink": 2.25,
+        "Drip Coffee": 2.29,
+        "Cold Brew": 3.65,
+        "Seasoned Fries": 2.99,
+        "Tater Tots": 2.99,
+        "Onion Rings": 2.99,
+        "Kettle Chips": 2.99
+    }
 
-menu_items = [
-    "Rev's Burger",
-    "Double Stack Cheese Burger",
-    "Classic Burger",
-    "Bacon Chesseburger",
-    "Three Tender Basket",
-    "Four Steak Finger Basket",
-    "Gig'Em Patty Melt", 
-    "Howdy Spicy Ranch Chicken Strip Sandwich",
-    "Classic Crispy or Grilled Chicken Tender Sandwich",
-    "Grilled Cheese",
-    "Aggie Shake",
-    "Double Scoop Ice Cream Cup",
-    "Chocolate Chip Chunk Cookie",
-    "Chocolate Fudge Brownie",
-    "Salad Bar",
-    "Gig'Em Sauce",
-    "Buffalo",
-    "Ranch",
-    "BBQ Sauce",
-    "Honey Mustard",
-    "Spicy Ranch",
-    "Fountain Drink",
-    "Drip Coffee",
-    "Cold Brew",
-    "Seasoned Fries",
-    "Tater Tots",
-    "Onion Rings",
-    "Kettle Chips"
-]
+    current_date = date.today() - timedelta(days=365)
+    hours = ["11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"]
 
-menu_prices = {
-    "Rev's Burger" : 5.59,
-    "Double Stack Cheese Burger" : 8.79,
-    "Classic Burger" : 5.49,
-    "Bacon Chesseburger" : 6.99,
-    "Three Tender Basket" : 6.79,
-    "Four Steak Finger Basket" : 7.29,
-    "Gig'Em Patty Melt" : 6.29,
-    "Howdy Spicy Ranch Chicken Strip Sandwich" : 6.99,
-    "Classic Crispy or Grilled Chicken Tender Sandwich" : 5.79,
-    "Grilled Cheese" : 3.49,
-    "Aggie Shake" : 3.99,
-    "Double Scoop Ice Cream Cup" : 2.99,
-    "Chocolate Chip Chunk Cookie" : 1.99,
-    "Chocolate Fudge Brownie" : 1.99,
-    "Salad Bar" : 8.99,
-    "Gig'Em Sauce" : 0.69,
-    "Buffalo" : 0.69,
-    "Ranch" : 0.69,
-    "BBQ Sauce" : 0.69,
-    "Honey Mustard" : 0.69,
-    "Spicy Ranch" : 0.69,
-    "Fountain Drink" : 2.25,
-    "Drip Coffee" : 2.29,
-    "Cold Brew" : 3.65,
-    "Seasoned Fries" : 2.99,
-    "Tater Tots" : 2.99,
-    "Onion Rings" : 2.99,
-    "Kettle Chips" : 2.99
-}
+    # 52 weeks in a year
+    for week in range(52):
 
-current_date = date.today() - timedelta(days=365)
-hours = ["11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm"]
+        # 7 work days in a week
+        for day in range(7):
+            orderID = 1
 
+            # 10 hours per work day
+            for hour in hours:
 
-# 52 weeks in a year
-for week in range(52):
+                # menu item orders per hour
+                for i in range(len(menu_items)):
+                    if (current_date.day == 31 and current_date.month == 8) or (current_date.day == 31 and current_date.month == 1):
+                        random_num = random.randint(5, 6)
+                    else:
+                        random_num = random.randint(0, 5)
 
-    # 7 work days in a week
-    for day in range(7):
-        orderID = 1
+                    # number of orders of a specific menu item
+                    for num in range(random_num):
+                        sql_file.write(f"INSERT INTO orders VALUES ('{hour}', {current_date.day}, {week}, {current_date.month}, {current_date.year}, '{menu_items[i]}', {menu_prices[menu_items[i]]}, {orderID});\n")
+                        orderID += 1
 
-        # 10 hours per work day
-        for hour in hours:
+            # changes date at the end of each day
+            current_date += timedelta(days=1)
 
-            # menu item orders per hour
-            for i in range(len(menu_items)):
-                if (current_date.day == 31 and current_date.month == 8) or (current_date.day == 31 and current_date.month == 1):
-                    random_num = random.randint(5, 6)
-                else:
-                    random_num = random.randint(0, 5)
-                
-                # number of orders of a specific menu item
-                for num in range(random_num):
-                    print(f"INSERT INTO orders VALUES ('{hour}', {current_date.day}, {week}, {current_date.month}, {current_date.year}, '{menu_items[i]}', {menu_prices[menu_items[i]]}, {orderID});")
-                    orderID += 1
-        
-        # changes date at the end of each day
-        current_date += timedelta(days=1)
-
-
-                
-
-    
-    
-
-
-inventory_items = {
+    inventory_items = {
     "Rev's Burger": ["2 Burger Bread", "Patty", "American Cheese", "Lettuce", "Tomato", "Gig'em Sauce", "Pickle"],
     "Double Stack Cheese Burger": ["Burger Bread", "2 Patty", "American Cheese", "2 American Cheese", "Lettuce", "Tomato", "Gig'Em Sauce", "Pickles"],
     "Classic Burger" : ["2 Burger Bread", "Patty", "American Cheese", "Lettuce", "Tomato", "Pickle"],
@@ -188,20 +176,20 @@ ingredients = [
     "Plastic Bags"
 ]
 
-print("CREATE TABLE ingredients (item varchar(255), ingredient varchar(255), count int);")
-print("CREATE TABLE inventory (ingredient varchar(255), amount int, capacity int);")
 
+sql_file.write("CREATE TABLE ingredients (item varchar(255), ingredient varchar(255), count int);\n")
+sql_file.write("CREATE TABLE inventory (ingredient varchar(255), amount int, capacity int);\n")
 
 for item in menu_items:
-    for ingredient in inventory_items[item]:
-        if ingredient[0] == "2":
-            print(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{2}');")
-        elif ingredient[0] == "3":
-            print(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{3}');")
-        elif ingredient[0] == "4":
-            print(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{4}');")
-        else:
-            print(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient}', '{1}');")
-        
+        for ingredient in inventory_items[item]:
+            if ingredient[0] == "2":
+                sql_file.write(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{2}');\n")
+            elif ingredient[0] == "3":
+                sql_file.write(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{3}');\n")
+            elif ingredient[0] == "4":
+                sql_file.write(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient[2:]}', '{4}');\n")
+            else:
+                sql_file.write(f"INSERT INTO ingredients VALUES ('{item}', '{ingredient}', '{1}');\n")
+
 for ingredient in ingredients:
-    print(f"INSERT INTO inventory VALUES ('{ingredient}', '{30}', '{30}');")
+        sql_file.write(f"INSERT INTO inventory VALUES ('{ingredient}', '{30}', '{30}');\n")
