147969b66014cfd6878e84b58943f55a6745e91c
Manisha Subrahmanya <manishatx@gmail.com>
2024-02-20 18:47:58 -0600

    added last two queries of the first four required ones

--- team04data/queries.sql
+++ team04data/queries.sql
@@ -17,3 +17,21 @@ FROM
     orders --get from the orders table
 GROUP BY 
     hour; --group it by hours
+
+'''"2 peak days": select top 10 sums of order total grouped by day in descending order by order total
+e.g. "30 August has $123456 of sales"'''
+SELECT day, month, year, SUM(sales)
+FROM orders
+GROUP BY day, month, year
+ORDER BY year, month, day
+LIMIT 10;
+
+'''"Inventory items for 20 menu items": select count of inventory items from inventory and menu grouped by menu item
+e.g. "chicken fingers uses 12 items"'''
+
+SELECT menu_item, COUNT(*) AS inventory_items_count    --  Counting the occurrences of each menu item in the Inventory table
+FROM Ingredients
+JOIN Inventory ON Ingredients.menu_item = Inventory.Ingredient  -- Joining Ingredients and Inventory tables on menu_item
+GROUP BY menu_item                                  -- Grouping the results by menu_item
+ORDER BY inventory_items_count DESC    -- Ordering the results by inventory_items_count in descending order
+LIMIT 20;            -- Limiting the output to the top 20 menu items with the highest inventory items count
