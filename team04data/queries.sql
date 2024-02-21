79b1069630a2e0cd12a856711e38edee4319eba8
RoxMS <roxanams@tamu.edu>
2024-02-20 20:58:56 -0600

    queries merged

--- team04data/queries.sql
+++ team04data/queries.sql
@@ -1,14 +1,14 @@
-'''"52 weeks of sales history": select the count of orders grouped by week
-e.g. "week 1 has 123 orders"'''
+-- '''"52 weeks of sales history": select the count of orders grouped by week
+-- e.g. "week 1 has 123 orders"'''
 SELECT
      week, COUNT(DISTINCT order_id) AS total_orders -- selects the week column from the restaurant_orders table and calculates the count of distinct order_id values for each week. 
 FROM 
-    orders --gets it from the orders table
+    orders --gets it from the orders table`
 GROUP BY week; --group each count by a week
 
 
-'''"Realistic sales history": select count of orders, sum of order total grouped by hour
-e.g. "12pm has 12345 orders totaling $86753"'''
+-- '''"Realistic sales history": select count of orders, sum of order total grouped by hour
+-- e.g. "12pm has 12345 orders totaling $86753"'''
 SELECT 
     hour,
     COUNT(DISTINCT order_id) AS total_orders, --count of the total orders based on their distinct ids and save it under total_orders for each hours
@@ -19,8 +19,8 @@ GROUP BY
     hour; --group it by hours
 
 
-'''"2 peak days": select top 10 sums of order total grouped by day in descending order by order total
-e.g. "30 August has $123456 of sales"'''
+-- '''"2 peak days": select top 10 sums of order total grouped by day in descending order by order total
+-- e.g. "30 August has $123456 of sales"'''
 SELECT day, month, year, SUM(sales)
 FROM orders
 GROUP BY day, month, year
@@ -28,8 +28,8 @@ ORDER BY year, month, day
 LIMIT 10;
 
 
-'''"Inventory items for 20 menu items": select count of inventory items from inventory and menu grouped by menu item
-e.g. "chicken fingers uses 12 items"'''
+-- '''"Inventory items for 20 menu items": select count of inventory items from inventory and menu grouped by menu item
+-- e.g. "chicken fingers uses 12 items"'''
 
 SELECT menu_item, COUNT(*) AS inventory_items_count    --  Counting the occurrences of each menu item in the Inventory table
 FROM Ingredients
@@ -39,8 +39,8 @@ ORDER BY inventory_items_count DESC    -- Ordering the results by inventory_item
 LIMIT 20;            -- Limiting the output to the top 20 menu items with the highest inventory items count
 
 
-'''"Top 10 Busiest Hours of the Day": select count of orders grouped by hour of the day
-e.g. "The top selling hour was 2pm"'''
+-- '''"Top 10 Busiest Hours of the Day": select count of orders grouped by hour of the day
+-- e.g. "The top selling hour was 2pm"'''
 
 SELECT 
     hour, --this selects the hour column from the orders table
@@ -54,15 +54,16 @@ ORDER BY
 LIMIT 10;            -- Limiting the output to the top 10 
 
 --Manisha
-'''"Highest sold item of the day"'''
-SELECT day, month, year, menu_item, COUNT(menu_item) as itemsSold
+-- '''"Highest sold item of the day"'''
+SELECT 
+    day, month, year, menu_item, COUNT(menu_item) as itemsSold
 FROM orders
 GROUP BY day, month, yeaR, menu_item
 ORDER BY year DESC, month DESC, day DESC, itemsSold DESC
 LIMIT 1;        
 
 
-'''"Highest sold item of the month"'''
+-- '''"Highest sold item of the month"'''
 SELECT month, year, menu_item, COUNT(menu_item) as itemsSold
 FROM orders
 GROUP BY month, year, menu_item
@@ -70,7 +71,7 @@ ORDER BY year DESC, month DESC, itemsSold DESC
 LIMIT 1;        
 
 
-'''"Most profitable month"'''
+-- '''"Most profitable month"'''
 SELECT month, year, SUM(sale) as totalSale          -- This selects the month and year calculates the sum of sales for each month and year, labeling this sum as totalSale
 FROM orders
 GROUP BY month, year                -- ensures that sales are aggregated for each specific month within each year
@@ -79,8 +80,8 @@ LIMIT 1;
 
 
 --Roxana
-'''"Highest Valued Order Sales": select count of orders grouped by hour of the day
-e.g. "The second largest sale total was $15.00"'''
+-- '''"Highest Valued Order Sales": select count of orders grouped by hour of the day
+-- e.g. "The second largest sale total was $15.00"'''
 
 SELECT 
     order_id, SUM(sale) AS total_sale --sums up the total sales
@@ -92,8 +93,8 @@ ORDER BY
     total_sale DESC; --sort them in descending order
 LIMIT 30;
 
-'''"Ingredient Items for a Specific Menu Item": select inventory items grouped by a menu item they correspond to
-e.g. "Revs Burger requires Burger Bread, a Patty, ...."'''
+-- '''"Ingredient Items for a Specific Menu Item": select inventory items grouped by a menu item they correspond to
+-- e.g. "Revs Burger requires Burger Bread, a Patty, ...."'''
 
 SELECT 
     ingredient, count --get the ingredient column and count column
