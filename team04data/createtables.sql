1c62badc7509d88e901930c6b3ec2323441d1405
jaymsteele <118499633+jaymsteele@users.noreply.github.com>
2024-02-21 18:38:09 -0600

    Cleans slate for creation of table in scripts

--- team04data/createtables.sql
+++ team04data/createtables.sql
@@ -1,6 +1,4 @@
 DROP TABLE orders;
 DROP TABLE ingredients;
 DROP TABLE inventory;
-CREATE TABLE orders (hour varchar(255), day int, week int, month int, year int, item varchar(255), sale int, orderID int);
-CREATE TABLE ingredients (item varchar(255), ingredient varchar(255), count int);
-CREATE TABLE inventory (ingredient varchar(255), amount int, capacity int);
+
