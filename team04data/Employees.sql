a78f0791396b4426e2cd8db2b39cac00696e44a8
jaymsteele <118499633+jaymsteele@users.noreply.github.com>
2024-02-27 08:39:14 -0600

    add employees table

---
+++ team04data/Employees.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Employees (
+    name TEXT,
+    id INT,
+    status TEXT
+);
+
+INSERT INTO Employees (name, id, status) VALUES ('David Wilson', 1784, 'manager');
+INSERT INTO Employees (name, id, status) VALUES ('Grace Wilson', 1737, 'manager');
+INSERT INTO Employees (name, id, status) VALUES ('David Davis', 9896, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('Eva Williams', 2560, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('Grace Williams', 2630, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('David Moore', 7725, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('Alice Williams', 2497, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('David Smith', 7042, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('Charlie Brown', 9043, 'employee');
+INSERT INTO Employees (name, id, status) VALUES ('Alice Jones', 1119, 'manager');
