75571f7be95ba0a8a768d9da6b02dd77668e9b14
RoxMS <122853180+RoxMS@users.noreply.github.com>
2024-02-27 10:41:36 -0600

    Update Sale.java with retrieve data from database and add to saleTable TableView

--- team04data/Sale.java
+++ team04data/Sale.java
@@ -28,3 +28,30 @@ public class Sale { //Represents each row
     }
 
 }
+
+@FXML private TableView<Sale> saleTable; //Declaration of TableView control to display Sale type items
+@FXML private TableColumn<Sale, Float> monthColumn; //Declaration of TableView column to display float type items
+@FXML private TableColumn<Sale, Float> yearColumn; //Declaration of TableView column to display float type items
+@FXML private TableColumn<Sale, Float> total_salesColumn; //Declaration of TableView column to display float type items
+
+
+TableColumn<Sale, Float> monthColumn = new TableColumn<>("Month");//Instantiate TableColumn with header text "Month"
+TableColumn<Sale, Float> yearColumn = new TableColumn<>("Year");//Instantiate TableColumn with header text "Year"
+TableColumn<Sale, Float> total_salesColumn = new TableColumn<>("Total Sales");//Instantiate TableColumn with header text "Total Sales"
+
+monthColumn.setCellValueFactory(new PropertyValueFactory<Sale, Float>("month")); //extracts data from month property of class
+yearColumn.setCellValueFactory(new PropertyValueFactory<Sale, Float>("year"));  //extracts data from year property of class
+total_salesColumn.setCellValueFactory(new PropertyValueFactory<Sale, Float>("total_sales"));//extracts data from total_sales property of class
+saleTable.setPlaceholder(new Label(" "));//Place holder for when table is empty
+
+try {//try statement to retrieve data from the "Orders" table in the database and add to saleTable Tableview
+    Statement stmt = conn.createStatement();
+    String sqlStatement = "SELECT month, year, SUM(sale) as totalSale FROM orders GROUP BY month, year ORDER BY year ASC, month ASC;"; //SQL query statement to grab data from database
+    ResultSet result = stmt.executeQuery(sqlStatement);
+    while (result.next()) {
+        String ingredient = result.getString("month");
+        float quanitity = result.getFloat("year");
+        float capacity = result.getFloat("totalSale");
+        saleTable.getItems().add(new Sale(month, year, total_sales) );
+    }
+}
