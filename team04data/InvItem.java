fbde045b3bc3e33b3f6f6eee725c364622d37e76
RoxMS <122853180+RoxMS@users.noreply.github.com>
2024-02-27 09:20:01 -0600

    Update InvItem.java with comments and finished class.

--- team04data/InvItem.java
+++ team04data/InvItem.java
@@ -1,10 +1,30 @@
 package application;
 
 public class InvItem { //each row
-    //columns
+    //Initialize Column variables
     private String item = null;
     private float quantity = 0.0f;
     private Float capacity = 0.0f;
 
+    //initialize each column to the parameter values in the constructor
+    public InvItem(String item, float quantity, float capacity) {
+        this.item = item;
+        this.quantity = quantity;
+        this.capacity = capacity;
+    }
+
+    //Getter for item column value
+    public String getItem() {
+        return item;
+    }
+    //Getter for quantity column value
+    public float getQuanity() {
+        return quantity;
+    }
+
+    //Getter for capacity column value
+    public float getCapacity() {
+        return capacity;
+    }
 
 }
