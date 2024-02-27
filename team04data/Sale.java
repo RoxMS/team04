b5ad4dead52fbeeecf12fa536aee662cc7a64ed5
RoxMS <122853180+RoxMS@users.noreply.github.com>
2024-02-27 09:11:18 -0600

    Update Sale.java with comments explaining the code.

--- team04data/Sale.java
+++ team04data/Sale.java
@@ -1,28 +1,28 @@
 package application;
 
-public class Sale { //each row
-    //columns
+public class Sale { //Represents each row
+    //Initialize Column variables
     private float month = 0.0f;
     private float year = 0.0f;
     private float total_sales = 0.0f;
 
-    //initialize each column
+    //initialize each column to the parameter values in the constructor
     public Sale(float month, float year, float total_sales) {
         this.month = month;
         this.year = year;
         this.total_sales = total_sales;
     }
 
-    //getter  column values
+    //Getter for month column value
     public float getMonth() {
         return month;
     }
-    //getter  column values
+    //Getter for year column value
     public float getYear() {
         return year;
     }
 
-    //getter  column values
+    //Getter for total sales column value
     public float getTotalSales() {
         return total_sales;
     }
