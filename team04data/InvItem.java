bb50379cf2bccd9112cfb0fcaebf700919c1240b
RoxMS <122853180+RoxMS@users.noreply.github.com>
2024-02-27 09:07:43 -0600

    Added InvItem.java and Sale.java files to main.

---
+++ team04data/InvItem.java
@@ -0,0 +1,10 @@
+package application;
+
+public class InvItem { //each row
+    //columns
+    private String item = null;
+    private float quantity = 0.0f;
+    private Float capacity = 0.0f;
+
+
+}
