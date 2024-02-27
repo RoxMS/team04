e61c9bf271558b526c2118ff18fe678a82261ca4
RoxMS <122853180+RoxMS@users.noreply.github.com>
2024-02-27 08:59:48 -0600

    Update Sale.java finished Sale class  with getters and initializers

--- team04data/Sale.java
+++ team04data/Sale.java
@@ -6,5 +6,25 @@ public class Sale { //each row
     private float year = 0.0f;
     private float total_sales = 0.0f;
 
+    //initialize each column
+    public Sale(float month, float year, float total_sales) {
+        this.month = month;
+        this.year = year;
+        this.total_sales = total_sales;
+    }
+
+    //getter  column values
+    public float getMonth() {
+        return month;
+    }
+    //getter  column values
+    public float getYear() {
+        return year;
+    }
+
+    //getter  column values
+    public float getTotalSales() {
+        return total_sales;
+    }
 
 }
