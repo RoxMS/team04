31523e46b4914f86dedfc6cb5cbe06b256867860
oliviafrancislee <115756966+oliviafrancislee@users.noreply.github.com>
2024-02-28 12:53:07 -0600

    added setters and id value

--- team04data/Order.java
+++ team04data/Order.java
@@ -1,18 +1,37 @@
 package application;
 
 public class Order {
+	//variables
+	private int orderID = 0;
 	private String menuItem = null;
 	private float price = 0.0f;
 	
-	public Order(String menuItem, float price) {
+	//initializer
+	public Order(int orderID, String menuItem, float price) {
+		this.orderID = orderID;
 		this.menuItem = menuItem;
 		this.price = price;
 	}
 	
+	//getters
+	public int getOrderID() {
+		return orderID;
+	}
 	public String getMenuItem() {
 		return menuItem;
 	}
 	public float getPrice() {
 		return price;
+	}
+	
+	//setters
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+	public void setMenuItem(String menuItem) {
+		this.menuItem = menuItem;
+	}
+	public void setPrice(float price) {
+		this.price = price;
 	}
 }
