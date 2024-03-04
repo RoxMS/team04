39e350446a12a8af587e2ab7be68d8a2e304ccf0
manishais <93087428+manishais@users.noreply.github.com>
2024-03-04 09:53:44 -0600

    Create Misc.java

---
+++ team04data/Misc.java
@@ -0,0 +1,21 @@
+package application.revs_pos_331;
+
+public class Misc {
+    //variables
+    private String menu_item = "";
+
+    //initializer
+    public Misc(String menu_item) {
+        this.menu_item = menu_item;
+    }
+
+    //getters
+    public String getMenu_Item() {
+        return menu_item;
+    }
+
+    //setters
+    public void setMenu_Item(String menu_item) {
+        this.menu_item =  menu_item;
+    }
+}
